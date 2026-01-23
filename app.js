dc83b62e520579e023534d7f213157141e298809
Gabriel Sá Teles Pires <gabriel.sateles.dev@gmail.com>
2026-01-23 00:17:14 -0300

    JogoDoNumeroSecreto

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 exibirMensagemIncicial();
-//ultimo teste
 
 // Função para exibir texto na tela e falar o texto em voz alta
 function exibirTextoNaTela(tag, texto) {
