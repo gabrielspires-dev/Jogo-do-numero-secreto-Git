95f22435c4c0bf9668e0af86b974a99eaab8cd84
Gabriel Sá Teles Pires <gabriel.sateles.dev@gmail.com>
2026-01-23 00:02:45 -0300

    updatednv

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 exibirMensagemIncicial();
-//TESTE
-console.log(`Número secreto (para testes): ${numeroSecreto}`);
+//TESTE bora ver se vai
 
 
 // Função para exibir texto na tela e falar o texto em voz alta
