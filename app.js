ed83a809a85ec9892fe11d4c83421dc036baf498
Gabriel Sá Teles Pires <gabriel.sateles.dev@gmail.com>
2026-01-23 00:05:16 -0300

    testup

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 exibirMensagemIncicial();
-//TESTE bora ver se vai
-
+//TESTE bora essa bomba
 
 // Função para exibir texto na tela e falar o texto em voz alta
 function exibirTextoNaTela(tag, texto) {
